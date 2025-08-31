b96e1ccc8e8de730eb4c6a2d6622bcc1dda531c1
Savaliya Manish <savaliyamanish1141@gmail.com>
2025-08-31 13:39:17 +0530

    Change Footer and Header

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="footer">
-    
-    <div class="left"><a href="https://github.com/schouffy/gamedev-portfolio" target="blank">Portfolio</a> by schouffy</div>
     <div class="right">Reach me at <a href="mailto:my@email.com">my@email.com</a> or <router-link to="/contact">through here</router-link></div>
   </div>
 </template>
