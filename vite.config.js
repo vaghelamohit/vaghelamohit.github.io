0a825236426ba989273ff14acc8dbf9bf2824a45
mohitghostt <mohitghostt@gmail.com>
2025-09-01 22:27:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
-export default ({mode}) => {
+export default ({ mode }) => {
   process.env = {
     ...process.env,
     ...loadEnv(mode, process.cwd())
@@ -11,12 +11,12 @@ export default ({mode}) => {
 
   return defineConfig({
     plugins: [vue()],
-    base:  '/',
+    base: '/Portfolio/',
     resolve: {
       alias: [
         // thanks to https://stackoverflow.com/a/67676242/22296059 :)
         { find: "@", replacement: fileURLToPath(new URL('./src', import.meta.url)) }
-      ] 
+      ]
     },
     css: {
       preprocessorOptions: {
