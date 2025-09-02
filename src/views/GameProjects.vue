ca33ec927808d59b33d2f6f68a59ff3e18a636da
mohitghostt <mohitghostt@gmail.com>
2025-09-02 19:01:06 +0530

    Update GameProjects.vue

--- src/views/GameProjects.vue
+++ src/views/GameProjects.vue
@@ -3,14 +3,10 @@
     <h1>My Games</h1>
 
     <div style="margin-bottom: 30px;">
-      The following are some stuff I've made or heavily contributed to.
+      The following are some games I've worked or heavily contributed to.
     </div>
 
     <ProjectsList v-bind:projects="projects"/>
-
-    <div style="margin-top: 20px;">
-      There is more to see on <a target="_blank" href="https://someexternalwebsite.com">some external website</a>
-    </div>
   </div>
 </template>
 
